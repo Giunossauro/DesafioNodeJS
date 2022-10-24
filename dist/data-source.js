43137a484e035d2c190f9de9c68bc78d0add6bd4
Giuliano Morelli <giuandroide@gmail.com>
2022-10-24 07:00:01 -0300

    make it better2

--- dist/data-source.js
+++ dist/data-source.js
@@ -4,12 +4,16 @@ exports.AppDataSource = void 0;
 require("dotenv/config");
 require("reflect-metadata");
 const typeorm_1 = require("typeorm");
-//const port = process.env.DB_PORT as number | undefined;
+const port = process.env.DB_PORT;
 exports.AppDataSource = new typeorm_1.DataSource({
     type: "postgres",
     url: process.env.DATABASE_URL,
+    host: process.env.DB_HOST,
+    port: port,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     synchronize: false,
-    logging: false,
     entities: [`${__dirname}/**/entities/*.{ts,js}`],
     migrations: [`${__dirname}/**/migrations/*.{ts,js}`]
 });
--- dist/migrations/1666599778306-default.js
+++ dist/migrations/1666599778306-default.js
@@ -8,6 +8,7 @@ class default1666599778306 {
     async up(queryRunner) {
         await queryRunner.query(`CREATE TABLE "images" ("id" SERIAL NOT NULL, "name" text NOT NULL, "description" text NOT NULL, "picture" text NOT NULL, "filename" text NOT NULL, CONSTRAINT "PK_1fe148074c6a1a91b63cb9ee3c9" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "users" ("id" SERIAL NOT NULL, "name" text NOT NULL, "email" text NOT NULL, "password" text NOT NULL, CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE EXTENSION PGCRYPTO`);
     }
     async down(queryRunner) {
         await queryRunner.query(`DROP TABLE "users"`);
