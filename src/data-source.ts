1eea872970e612633d5e49300502e167bc7eb758
Giuliano Morelli <giuandroide@gmail.com>
2022-10-24 04:41:27 -0300

    set for heruku

--- src/data-source.ts
+++ src/data-source.ts
@@ -2,16 +2,13 @@ import "dotenv/config";
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 
-const port = process.env.DB_PORT as number | undefined;
+//const port = process.env.DB_PORT as number | undefined;
 
 export const AppDataSource = new DataSource({
 	type: "postgres",
-	host: process.env.DB_HOST,
   url: process.env.DATABASE_URL,
-	port: port,
-	username: process.env.DB_USERNAME,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DB_DATABASE,
+  synchronize: false,
+  logging: false,
 	entities: [`${__dirname}/**/entities/*.{ts,js}`],
-	migrations: [`${__dirname}/**/migrations/*.{ts,js}`],
+	migrations: [`${__dirname}/**/migrations/*.{ts,js}`]
 });
